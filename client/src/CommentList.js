3c74780b5366775ba78a7bb6bd56b8fdc2582f54
Adrian Ponce <adrian.ponce.89@gmail.com>
2022-09-05 12:54:36 -0300

    Use query service in client

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,21 +1,6 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import CommentCreate from "./CommentCreate";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchCommentss = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchCommentss();
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
