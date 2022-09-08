1bf00a1d4f354cc1f4abe69c7c55276a65c0a659
Adrian Ponce <adrian.ponce.89@gmail.com>
2022-09-08 19:52:07 -0300

    Add moderation service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,29 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "This comment is awating moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+    const style = {
+      color:
+        comment.status === "pending"
+          ? "green"
+          : comment.status === "rejected"
+          ? "red"
+          : "black",
+    };
+    return (
+      <li key={comment.id} style={style}>
+        {content}
+      </li>
+    );
   });
 
   return <ul>{renderedComments}</ul>;
--- query/index.js
+++ query/index.js
@@ -22,12 +22,25 @@ app.post("/events", (req, res) => {
     };
   }
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
     posts[postId].comments.push({
       id,
       content,
+      status,
     });
   }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.content = content;
+    comment.status = status;
+  }
+
   res.send({});
 });
 
